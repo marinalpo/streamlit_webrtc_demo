58fd2442807ad915dc4bd2de9dd00f458fc2fafc
Marina Alonso Poal <43316350+marinalpo@users.noreply.github.com>
2022-08-18 17:31:29 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# demo-teaser-living-lab
+
+Creation of an interactive demo for the teaser of the Living Lab in the Sea Towers that consists on a Real-Time Emotion Recognition application.
---
+++ main_app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from PIL import Image
+import av
+import numpy as np
+import cv2
+import mediapipe as mp
+from deepface import DeepFace
+import time
+from streamlit_webrtc import (
+    RTCConfiguration,
+    WebRtcMode,
+    WebRtcStreamerContext,
+    webrtc_streamer,
+)
+
+
+def callback(frame: av.VideoFrame) -> av.VideoFrame:
+    img = frame.to_ndarray(format="bgr24")
+    img = np.flip(img, axis=1).astype(np.uint8)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+## PAGE CONFIGURATION -------------------------------------------------------------------------------------------------
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+st.set_page_config(
+       layout='centered',  # 'centered' or 'wide'
+       initial_sidebar_state="collapsed",
+       menu_items=None)
+
+cols = st.columns([1, 3, 1])
+cols[1].write(' ')
+
+st.markdown('WebRTC Demo', unsafe_allow_html=True)
+
+webrtc_ctx = webrtc_streamer(
+    key="emotion_recognition",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    desired_playing_state=True,
+    video_frame_callback=callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True
+)
